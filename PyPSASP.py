1ba15a308d94bb0205d5f2645b461178d9b54b7b
liu2bao <liu2bao@yeah.net>
2019-04-15 14:54:21 +0800

    Initial commit

---
+++ PyPSASP.py
@@ -0,0 +1,16 @@
+import os
+
+class PSASP(object):
+    @property
+    def path_temp(self):
+        return self._path_temp
+
+    @path_temp.setter
+    def path_temp(self, value):
+        if not isinstance(value, str):
+            raise ValueError('path_temp must be a string!')
+        self._path_temp = value
+
+
+    def __init__(self,path_temp):
+        self.path_temp = path_temp
