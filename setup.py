8d525d8546bef45d56da7c2c9ea64fa1f84bf8d0
liu2bao <liu2bao@yeah.net>
2019-04-26 22:20:51 +0800

    Modified the version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyPSASP",
-    version="0.1.1",
+    version="0.1.2",
     author="liu2bao",
     author_email="liu2bao@yeah.net",
     description="Use python to manipulate PSASP",
